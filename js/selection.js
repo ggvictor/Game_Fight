60bf9fcc7209a0cf1d2db6c9d3e3ad289f2a6062
ggvictor <victorrodriguessilva2@gmail.com>
2026-02-10 19:18:37 -0300

    começando os objetos dos personagens

--- js/selection.js
+++ js/selection.js
@@ -1,7 +1,62 @@
+const personagens = [
+  {
+    nome:"Luffy",
+    força:Math.floor(Math.random()*70+5),
+    inteligencia:Math.floor(Math.random()*50+10),
+    agilidade:Math.floor(Math.random()*70+10),
+    mp:Math.floor(Math.random()*70+10),
+    profile: "img/luffy_fight.png"
+  },
+  {
+    nome:"Naruto",
+    força:Math.floor(Math.random()*70+10),
+    inteligencia:Math.floor(Math.random()*60+10),
+    agilidade:Math.floor(Math.random()*50+3),
+    mp:Math.floor(Math.random()*70+10),
+    profile: "img/naruto_fight.png"
+  },
+  {
+    nome:"Ichigo",
+    força:Math.floor(Math.random()*80+10),
+    inteligencia:Math.floor(Math.random()*70+5),
+    agilidade:Math.floor(Math.random()*50+1),
+    mp:Math.floor(Math.random()*60+5),
+    profile: "img/ichigo_fight.png"
+  },
+  {
+    nome:"gon",
+    força:Math.floor(Math.random()*70+1),
+    inteligencia:Math.floor(Math.random()*70+10),
+    agilidade:Math.floor(Math.random()*80+1),
+    mp:Math.floor(Math.random()*65+10),
+    profile: "img/gon_fight.png"
+  },
+  {
+    nome:"Itadori",
+    força:Math.floor(Math.random()*80+5),
+    inteligencia:Math.floor(Math.random()*50+10),
+    agilidade:Math.floor(Math.random()*60+3),
+    mp:Math.floor(Math.random()*80+5),
+    profile: "img/itadori_fight.png"
+  },
+]
+
 const img = document.querySelectorAll(".img");
+const btn_next = document.querySelector("#btn_next")
 
 img.forEach((i) => {
   i.addEventListener("click", () => {
-    i.style.border = "3px solid white";
+    i.classList.toggle("border")
+    choosePlayers();
   });
 });
+
+const choosePlayers = ()=>{
+  const imgChecked = document.querySelectorAll(".border")
+  if(imgChecked.length === 2){
+    btn_next.style.display = "flex";
+  }
+  else{
+    btn_next.style.display = "none";
+  }
+}
